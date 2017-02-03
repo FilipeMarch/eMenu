a12b5fa0bf4f30faa7314b30d9e143e9a7698c9c
FilipeMarch <lipe_march@hotmail.com>
2017-02-03 06:27:46 -0300

    Adicionado alguns comentários

--- eMenu/app/src/main/java/br/com/galdir/emenu/MainActivity.java
+++ eMenu/app/src/main/java/br/com/galdir/emenu/MainActivity.java
@@ -18,17 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         //Cria uma lista de itens na tela principal do app
         String[] itens = {"Comidas", "Bebidas", "Sobremesas"};
-
         ListView listaDeItens = (ListView) findViewById(R.id.lista_itens);
-
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itens);
-
         listaDeItens.setAdapter(adapter);
 
 
         //referência para o botão e associado a ele um listener para o evento de clique
-
         Button novoItem = (Button) findViewById(R.id.novo_item);
         novoItem.setOnClickListener(new View.OnClickListener(){
             @Override
--- eMenu/app/src/main/java/br/com/galdir/emenu/NovoItemActivity.java
+++ eMenu/app/src/main/java/br/com/galdir/emenu/NovoItemActivity.java
@@ -16,16 +16,15 @@ public class NovoItemActivity extends AppCompatActivity {
 
         //uma referência para o botão Salvar
         Button botaoSalvar = (Button) findViewById(R.id.item_salvar);
-
         //listener para o evento de clique nesse botão:
         botaoSalvar.setOnClickListener(new View.OnClickListener(){
             @Override
+
+            //comportamento desejado para o botão
             public void onClick(View view) {
-                //comportamento desejado para o botão
 
                 //texto que aparece ao clicar no botão
                 Toast.makeText(NovoItemActivity.this, "Item adicionado", Toast.LENGTH_SHORT).show();
-
                 //retorna para a tela principal
                 Intent intent = new Intent(NovoItemActivity.this, MainActivity.class);
                 startActivity(intent);
