f4aeaf8f0ab82bb1147ba6505e8c7ed87329be0c
FilipeMarch <lipe_march@hotmail.com>
2017-02-03 05:55:09 -0300

    Adicionado botão novo item, retangular Adicionada a função do botão - transitar entre as telas de preenchimento de novos itens e o cardápio em si

--- eMenu/app/src/main/java/br/com/galdir/emenu/MainActivity.java
+++ eMenu/app/src/main/java/br/com/galdir/emenu/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.galdir.emenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +25,23 @@ public class MainActivity extends AppCompatActivity {
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itens);
 
         listaDeItens.setAdapter(adapter);
+
+
+        //referência para o botão e associado a ele um listener para o evento de clique
+
+        Button novoItem = (Button) findViewById(R.id.novo_item);
+        novoItem.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view) {
+                //objetivo do click será...
+                Intent intent = new Intent(MainActivity.this, NovoItemActivity.class);
+                startActivity(intent);
+
+            }
+
+
+
+        });
 
 
 
