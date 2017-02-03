9b38bac502c5e08ede7d2f6bf67bf7c1facbc404
FilipeMarch <lipe_march@hotmail.com>
2017-02-03 04:57:07 -0300

    Adicionado IDs para os campos dos itens Adicionada mensagem ao clicar no botão salvar

--- eMenu/app/src/main/java/br/com/galdir/emenu/NovoItemActivity.java
+++ eMenu/app/src/main/java/br/com/galdir/emenu/NovoItemActivity.java
@@ -2,6 +2,9 @@ package br.com.galdir.emenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NovoItemActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class NovoItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_item);
+
+        //uma referência para o botão Salvar
+        Button botaoSalvar = (Button) findViewById(R.id.item_salvar);
+
+        //listener para o evento de clique nesse botão:
+        botaoSalvar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //comportamento desejado para o botão
+
+                Toast.makeText(NovoItemActivity.this, "Item adicionado", Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
     }
+
+
 }
