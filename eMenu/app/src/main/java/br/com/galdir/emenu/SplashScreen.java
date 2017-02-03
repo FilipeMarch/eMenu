f51b751b591c605954cd174909445ecfc4eb6ede
FilipeMarch <lipe_march@hotmail.com>
2017-02-03 06:21:34 -0300

    Adicionada Splash Screen - imagem ao iniciar aplicativo, delay de 1000ms até desaparecer

---
+++ eMenu/app/src/main/java/br/com/galdir/emenu/SplashScreen.java
@@ -0,0 +1,44 @@
+package br.com.galdir.emenu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+class SplashActivity extends MainActivity
+
+{
+    private static final long DELAY = 1000;
+    private boolean scheduled = false;
+    private Timer splashTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        splashTimer = new Timer();
+        splashTimer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                SplashActivity.this.finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, DELAY);
+        scheduled = true;
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if (scheduled)
+            splashTimer.cancel();
+        splashTimer.purge();
+    }
+}
